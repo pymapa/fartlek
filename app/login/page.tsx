6c35cbcacdd7cf314d1b82b442a9f47cacfbbdf4
Pyry Pajunpää <pyry@identio.fi>
2024-02-22 15:30:45 +0200

    Initial commit, login layout

---
+++ app/login/page.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import stravaIcon from "../../public/icons/strava-icon.svg";
+import Image from "next/image";
+
+const Login = () => {
+  return (
+    <div className="flex w-screen h-screen flex-wrap md:flex-nowrap">
+      <Sidebar />
+      <LoginForm />
+    </div>
+  );
+};
+
+const LoginForm = () => {
+  return (
+    <div className="w-screen md:w-4/6">
+      <div className="hero h-16 min-h-full md:min-h-screen">
+        <div className="hero-content w-full text-center flex flex-col flex-">
+          <h1
+            className="text-3xl
+            text-accent"
+          >
+            Sign in
+          </h1>
+          <button className="btn mt-4">
+            Sign in with Strava
+            <Image src={stravaIcon} alt="Strava" width={25} />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Sidebar = () => {
+  return (
+    <div className="w-screen md:w-2/6 p-4 bg-gradient-to-br from-accent-content to-accent">
+      <div className="hero h-10 min-h-full md:min-h-screen">
+        <div className="hero-content text-center">
+          <div className="max-wd-md">
+            <h1 className="text-5xl text-base-100">Welcome to Fartlek!</h1>
+            <p className="py-6 text-base-300">
+              Do what you want with your activity data
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Login;
